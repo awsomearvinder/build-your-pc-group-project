621a9239222c80585810b67393872abfef1e7c0f
Arvinder Dhanoa <ArvinderDhan@gmail.com>
2024-03-29 18:02:52 -0500

    chore: run `black` formatter on code.

--- build-your-pc/main.py
+++ build-your-pc/main.py
@@ -1,18 +1,22 @@
 from quart import Quart, render_template, websocket, send_from_directory
 
-app = Quart(__name__, static_folder = None, static_url_path = None)
+app = Quart(__name__, static_folder=None, static_url_path=None)
+
 
 @app.route("/")
 async def hello():
     return await render_template("index.html")
 
+
 @app.route("/store/<path:path>")
 async def store(path):
-    return await send_from_directory('store', path)
+    return await send_from_directory("store", path)
+
 
 @app.route("/static/<path:path>")
 async def static(path):
-    return await send_from_directory('static', path)
+    return await send_from_directory("static", path)
+
 
 if __name__ == "__main__":
     app.run()
