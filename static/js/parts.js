be74ab0bfcd6cc01c2f059492a3b423b7f73bddd
sf4925lx <nsoranno10@yahoo.com>
2024-04-25 01:56:43 -0500

    Add and remove parts from list. Add save/update list functions.

--- static/js/parts.js
+++ static/js/parts.js
@@ -8,6 +8,8 @@ function onLoadDashboard(component) {
     window.location.href = '/products/' + component;
 }
 
+let compProduct;
+
 function onLoadPartList(component) {
     fetchPartData(component).then((data) => {
         if (component === "memory") {
@@ -20,6 +22,7 @@ function onLoadPartList(component) {
                 }
             })
         }
+        compProduct = component;
         formatPartsTable(data);
     })
 }
@@ -38,21 +41,39 @@ function formatPartsTable(data) {
     }
     tableString = tableString + "<th class='price-column'>Price</th><th class='add-column'></th></tr></thead><tbody>";
     for (let i = 0; i < 50; i++) {
-        const name = data[i].name;
-        delete data[i].name;
-        tableString = tableString + "<tr><td class='name-cell'>" + name + "</td>";
+        const filteredData = data[i];
+        const name = filteredData.name;
+        delete filteredData.name;
+        tableString = tableString + "<tr id='selection__" + i + "'><td class='name-cell'>" + name + "</td>";
         // const price = data[i].price ? data[i].price : "-";
         // delete data[i].price;
-        for (let prop in data[i]) {
-            const value = data[i][prop] ?? "-";
-            tableString = tableString + "<td>" + value + "</td>";
+        for (let prop of Object.values(filteredData)) {
+            if (prop === 1 || prop === 0) {
+                const value = prop === 1 ? "Yes" : "No";
+                tableString = tableString + "<td>" + value + "</td>";
+            } else {
+                const value = prop ?? "-";
+                tableString = tableString + "<td>" + value + "</td>";
+            }
+
         }
         tableString = tableString + "<td class='price-cell'></td>";
-        tableString = tableString + "<td><button class='btn btn--full'>Add</button></td></tr>";
+        tableString = tableString + "<td><button class='btn btn--full' onclick='addPartToList("+i+")'>Add</button></td></tr>";
     }
     tableString = tableString + "</tbody>";
     $('#parts-list').html(tableString);
+}
 
+function addPartToList(i) {
+    const value = $('#selection__' + i + '> .name-cell').html();
+    localStorage.setItem(compProduct, value);
+    window.location.href = '/parts-dashboard';
+}
+
+function removeSelection(key) {
+    localStorage.removeItem(key.id);
+    $(key).children('.td-selection').html("<button class='btn btn--full' onclick='onLoadDashboard(\""+key.id+"\")'>+ Select</button>");
+    $(key).children('.td-remove').remove();
 }
 
 function convertKeyRegex(str) {
@@ -68,12 +89,51 @@ async function fetchPartData(component) {
     return $.ajax({
         url: url,
         method: "GET",
-        dataType: "json",
-        success: function (res) {
-            return res;
-        },
-        error: function (err) {
-            console.log(err);
-        }
+        dataType: "json"
     });
+}
+
+function saveList() {
+    const data = fetchData(null);
+    if (data.component !== null) {
+        $.ajax({
+            url: "/save-list",
+            method: "POST",
+            dataType: "json",
+            data: JSON.stringify(data)
+        }).done(function(data) {
+            console.log(data);
+        })
+    }
+}
+
+function updateList() {
+    let id = $('#build-list').prop('selectedIndex');
+    const data = fetchData(id);
+    if (data.component !== null) {
+        $.ajax({
+            url: "/save-list",
+            method: "POST",
+            dataType: "json",
+            data: JSON.stringify(data)
+        }).done(function(data) {
+            console.log(data);
+        })
+    }
+}
+
+function fetchData(id) {
+    let data = {
+        "component": {
+        },
+        "id": id
+    }
+    for (let i = 0; i < localStorage.length; i++) {
+        if (localStorage.key(i)) {
+            const key = localStorage.key(i);
+            const value = localStorage.getItem(key);
+            data["component"][key] = value;
+        }
+    }
+    return data;
 }
